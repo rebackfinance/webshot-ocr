5cc834586ef9e177b36c291c34b59a001ea819c6
someone <onlyiqquee@gmail.com>
2024-04-03 11:32:26 +0100

    selenium.StartFrameBuffer() commented out

---
+++ example/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	webshot "github.com/rebackfinance/webshot-ocr"
+)
+
+func main() {
+	config := webshot.NewConfig{
+		Address:     "http://localhost",
+		Port:        4444, // you can change accordingly to which ever port you wish
+		BrowserName: webshot.FirefoxBrowser,
+		DebugMode:   true,                  // set to true if you want to get the logs
+		DriverPath:  "./creds/geckodriver", // your gekodriver path goes in here
+	}
+
+	driver, err := webshot.NewWebshot(config)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	url := "https://google.com"
+
+	byteImage, err := driver.Screenshot(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fileName := "screenshot" + ".png"
+	pngData, _ := os.Create(fileName)
+	pngData.Write([]byte(byteImage))
+}
